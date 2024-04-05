fb42d6798959daa7f649fed38c85b7ee836d0e6f
Noorulain <Noorulainmemon111@gmail.com>
2024-04-05 23:40:30 +0500

    brandpage

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+function App() {
+  return (
+    
+       <div>
+
+      <Navbar/>
+      <Hero/> 
+    </div>
+  );
+}
+
+export default App;
